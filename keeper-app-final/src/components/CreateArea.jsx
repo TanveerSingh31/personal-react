e3772982fcbbb71add255d0d42f9a62496a4bbd9
TanveerSingh01 <tanveer.singh1@incedoinc.com>
2023-12-10 16:14:52 +0530

    Integrated Material UI into App

--- keeper-app-final/src/components/CreateArea.jsx
+++ keeper-app-final/src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
 
@@ -7,23 +10,29 @@ function CreateArea(props) {
     content: ""
   });
 
+  const [ writing, setWriting ] = useState(false);
+
   function ChangeText(e){
     let {name, value} = e.target; 
     setText((prevValue)=>{
-      return { ...prevValue, [name]: value}
+      return { ...prevValue, [name]: value}  
     });
   }
 
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" onChange={ChangeText} value={text.title}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" onChange={ChangeText} value={text.content}/>
-        <button onClick={(e)=>{
-          props.addTask(text);
-          setText({title:"", content:""});
-          e.preventDefault();
-        }}>Add</button>
+      <form className="create-note">
+        {writing && <input name="title" placeholder="Title" onChange={ChangeText} value={text.title}/>}
+        <textarea name="content" placeholder="Take a note..." rows={ writing ? "3" : "1"} onChange={ChangeText} value={text.content} onClick={()=>{ setWriting(true); }}/>
+       {writing && <zoom in={writing}>
+        <Fab onClick={(e)=>{
+            props.addTask(text);
+            setText({title:"", content:""});
+            e.preventDefault();
+          }}><AddIcon />
+        </Fab>
+       </zoom>}
+        
       </form>
     </div>
   );
